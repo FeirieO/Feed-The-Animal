864f54bf51bb8ae7d412c3e2341f5e6c79b2e773
feirie001@gmail.com <feirie001@gmail.com>
2022-08-27 20:25:38 +0100

    Add project files.

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float cooldown;
+
+    void Update()
+    {
+        cooldown = cooldown - Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cooldown < 0)
+            {
+                Invoke("SpawnDog", 0);
+            }
+
+        }
+    }
+
+    private void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        cooldown = 2;
+    }
+}
